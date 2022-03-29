c9d6478dd742018eb27fdefaa6d563f7e613204d
Mike The Madkiwi <mike@hildyard.ca>
2022-03-29 10:00:22 -0400

    SHOULD update casino interiors

--- client.lua
+++ client.lua
@@ -17,11 +17,26 @@ local MKDEBUGSHOW = false -- shows polyzone and onscreen closest door perms
 Citizen.CreateThread(function()
     RequestAllIpls()
 end)
+function ToggleEntitySet(interior, entitySet, toggle)
+    if toggle then
+        ActivateInteriorEntitySet(interior, entitySet, true)
+    else
+        DeactivateInteriorEntitySet(interior, entitySet, false)
+    end
+end
 function RequestAllIpls()
+    Citizen.Wait(1)
+    loadMainIPL()
+    Citizen.Wait(1)
+    loadVault()
+    Citizen.Wait(1)
+    Print('Loaded Casino IPL')
+    -- ActivateInteriorEntitySet(274689, "Set_Pent_Arcade_Retro")  
+end
+function loadMainIPL()
     RequestIpl("vw_casino_main");
     RequestIpl("vw_casino_carpark");
     RequestIpl("vw_casino_garage");
-    RequestIpl("vw_casino_penthouse");
     RequestIpl("hei_dlc_casino_aircon");
     RequestIpl("hei_dlc_casino_aircon_lod");
     RequestIpl("hei_dlc_casino_door");
@@ -34,12 +49,150 @@ function RequestAllIpls()
     RequestIpl("vw_ch3_additions_strm_0");
     RequestIpl("vw_dlc_casino_door");
     RequestIpl("vw_dlc_casino_door_lod");
+    RequestIpl("vw_int_placement_vw");
     RequestIpl("vw_casino_billboard");
     RequestIpl("vw_casino_billboard_lod(1)");
     RequestIpl("vw_casino_billboard_lod");
-    RequestIpl("vw_int_placement_vw");
-    RequestIpl("vw_dlc_casino_apart");  
-    ActivateInteriorEntitySet(274689, "Set_Pent_Arcade_Retro")  
+    RequestIpl("vw_casino_ch3_additions")
+    RequestIpl("vw_casino_ch3_additions_long_0")
+    RequestIpl("vw_casino_ch3_additions_strm_0")
+end
+function loadVault()
+    local interiorid = GetInteriorAtCoords(2488.348, -267.3637, -71.64563)
+    ToggleEntitySet(interiorid, "set_vault_door", true) -- Open vault
+    ToggleEntitySet(interiorid, "set_spawn_group1", true) -- small locker on the sides and arts
+    -- ToggleEntitySet(interiorid, "set_spawn_group2", false) -- small locker on the sides and arts
+    ToggleEntitySet(interiorid, "set_vault_dressing", true) -- all the stuff that spawns on the floor
+    -- ToggleEntitySet(interiorid, "set_vault_cash_01", true)
+    ToggleEntitySet(interiorid, "set_vault_cash_02", true)
+    ToggleEntitySet(interiorid, "set_vault_art_01", true)
+    -- ToggleEntitySet(interiorid, "set_vault_art_02", true)
+    ToggleEntitySet(interiorid, "set_vault_gold_01", true)
+    ToggleEntitySet(interiorid, "set_vault_gold_02", true)
+    ToggleEntitySet(interiorid, "set_vault_diamonds_01", true)
+    ToggleEntitySet(interiorid, "set_vault_diamonds_02", true)
+    -- ToggleEntitySet(interiorid, "set_vault_door_lockedxd", false) -- Locked vault door
+    -- ToggleEntitySet(interiorid, "set_vault_door_broken", false) -- Vault door exloded/broken
+    -- ToggleEntitySet(interiorid, "set_vault_wall_damagedxd", false) -- Vault wall damaged -- always broken?
+    RefreshInterior(interiorid)
+end
+function loadPenthouse()
+    RequestIpl("vw_casino_penthouse");
+    RequestIpl("vw_dlc_casino_apart");
+    local interiorid = GetInteriorAtCoords(976.63640000, 70.29476000, 115.16410000)
+    local color = 3
+    ToggleEntitySet(interiorid, "casino_manager_default", true)
+    ToggleEntitySet(interiorid, "casino_manager_workout", true)
+    ToggleEntitySet(interiorid, "set_pent_tint_shell", true)
+    SetInteriorEntitySetColor(interiorid, "set_pent_tint_shell", color)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_spax_shell", true)
+    SetInteriorEntitySetColor(interiorid, "vw_vwint02_pent_spax_shell", color)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_shell", true)
+    SetInteriorEntitySetColor(interiorid, "vw_vwint02_pent_spa_shell", color)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_sbt_shell", true)
+    SetInteriorEntitySetColor(interiorid, "vw_vwint02_pent_sbt_shell", color)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_mbt_shell", true)
+    SetInteriorEntitySetColor(interiorid, "vw_vwint02_pent_mbt_shell", color)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_hal_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_mb_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_lou_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_sb_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_din_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_bar_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_off_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_lv_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_ex_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_spax_window", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_window", true)
+
+    ToggleEntitySet(interiorid, "set_pent_media_bar_open", true)
+    -- ToggleEntitySet(interiorid, "set_pent_media_bar_closed", true)
+
+    ToggleEntitySet(interiorid, "set_pent_spa_bar_open", true)
+    -- ToggleEntitySet(interiorid, "set_pent_spa_bar_closed", true)
+
+    ToggleEntitySet(interiorid, "set_pent_dealer", true)
+    -- ToggleEntitySet(interiorid, "set_pent_nodealer", true)
+
+    -- local pattern = "set_pent_pattern_01"
+    -- local pattern = "set_pent_pattern_02"
+    -- local pattern = "set_pent_pattern_03"
+    -- local pattern = "set_pent_pattern_04"
+    -- local pattern = "set_pent_pattern_05"
+    -- local pattern = "set_pent_pattern_06"
+    -- local pattern = "set_pent_pattern_07"
+    -- local pattern = "set_pent_pattern_08"
+    local pattern = "set_pent_pattern_09"
+    ToggleEntitySet(interiorid, pattern, true)
+    SetInteriorEntitySetColor(interiorid, pattern, color)
+
+    ToggleEntitySet(interiorid, "set_pent_arcade_retro", true)
+    -- ToggleEntitySet(interiorid, "set_pent_arcade_modern", true)
+
+    -- ToggleEntitySet(interiorid, "set_pent_clutter_01") -- trash on the floor
+    -- ToggleEntitySet(interiorid, "set_pent_clutter_02") -- trash on the floor
+    -- ToggleEntitySet(interiorid, "set_pent_clutter_03") -- trash on the floor
+    -- ToggleEntitySet(interiorid, "set_pent_bar_clutter") -- trash on the bar
+
+    -- ToggleEntitySet(interiorid, "set_pent_lounge_blocker") -- hitbox to block doors
+    -- ToggleEntitySet(interiorid, "set_pent_guest_blocker") -- hitbox to block doors
+    -- ToggleEntitySet(interiorid, "set_pent_office_blocker") -- hitbox to block doors
+    -- ToggleEntitySet(interiorid, "set_pent_cine_blocker") -- hitbox to block doors
+    -- ToggleEntitySet(interiorid, "set_pent_spa_blocker") -- hitbox to block doors
+    -- ToggleEntitySet(interiorid, "set_pent_bar_blocker") -- hitbox to block doors
+
+    -- ToggleEntitySet(interiorid, "set_pent_bar_party_after", true)
+    -- ToggleEntitySet(interiorid, "set_pent_bar_party_0", true)
+    -- ToggleEntitySet(interiorid, "set_pent_bar_party_1", true)
+    ToggleEntitySet(interiorid, "set_pent_bar_party_2", true)
+
+    ToggleEntitySet(interiorid, "set_pent_bar_light_0", true)
+    ToggleEntitySet(interiorid, "set_pent_bar_light_01", true)
+    ToggleEntitySet(interiorid, "set_pent_bar_light_02", true)
+
+    -- TVs
+    ToggleEntitySet(interiorid, "prop_tv_flat_03b", true)
+    ToggleEntitySet(interiorid, "prop_tv_flat_michael", true)
+    ToggleEntitySet(interiorid, "prop_tv_flat_01", true)
+    ToggleEntitySet(interiorid, "vw_prop_vw_cinema_tv_01", true)
+    ToggleEntitySet(interiorid, "v_res_tt_tvremote", true)
+    ToggleEntitySet(interiorid, "vw_prop_vw_tv_rt_01a", true)
+
+    -- Mirrors
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_mbt_mirror", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_mbx_drframe", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_loux_mirror", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_sbt_mirror", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_wk_mirror", true)
+
+    -- edge blends
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_mb_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_hal_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_lou_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_sb_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_din_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_bar_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_off_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_wk_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_lv_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_edgeblend", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_cin_edgeblend", true)
+
+    ToggleEntitySet(interiorid, "vw_prop_vw_wallart_47a", true)
+    -- ToggleEntitySet(interiorid, "vw_prop_vw_safedoor_office2a_l", true)
+    -- ToggleEntitySet(interiorid, "vw_prop_vw_safedoor_office2a_r", true)
+
+    -- ToggleEntitySet(interiorid, "prop_box_guncase_01a", true)
+    -- ToggleEntitySet(interiorid, "prop_box_guncase_02a", true)
+
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_water_01", true)
+    -- ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_water_02", true)
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_spa_water_03", true)
+
+    ToggleEntitySet(interiorid, "vw_vwint02_pent_signs", true)
+
+    RefreshInterior(interiorid)
+
 end
 -----------------------------------------
 local isInZone = false
@@ -410,3 +563,18 @@ RegisterCommand('mkallcasinoinfo', function(source, args)
     print('zones being handled:'..zonecount..'') 
     print('doors being handled:'..doorcount..'') 
 end,false)
+
+
+
+
+
+    -- RequestIpl("ch_int_placement_ch_interior_0_dlc_casino_heist_milo_")
+    -- apparently no interior_1_ ??
+    -- RequestIpl("ch_int_placement_ch_interior_2_dlc_plan_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_3_dlc_casino_back_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_4_dlc_casino_hotel_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_5_dlc_casino_loading_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_6_dlc_casino_vault_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_7_dlc_casino_utility_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_8_dlc_tunnel_milo_")
+    -- RequestIpl("ch_int_placement_ch_interior_9_dlc_casino_shaft_milo_")
